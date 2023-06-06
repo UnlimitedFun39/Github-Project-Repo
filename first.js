2e49d7604609f1d5d9c7da99acf1986a83d68368
AL SHAHRIAR HOSSAIN <https://github.com/UnlimitedFun39/My-First-Repo.git>
2023-06-06 22:42:09 +0600

    Js added

---
+++ first.js
@@ -0,0 +1,112 @@
+// var price = 200;
+// var age = 20;
+// console.log(price + age);
+
+// var main = 10;
+// main++;
+// console.log(main);
+
+// var name1 = "Ashif";
+// var Name = "Shahriar";
+
+// console.log(name1 + " " + Name);
+
+// var name = "100";
+
+// var out = parseInt(name);
+
+// console.log(out);
+
+// var Name = 0.1;
+// var nane1 = 0.2;
+// var res = Name + nane1;
+// res = res.toFixed(2);
+// res = parseFloat(res);
+// console.log(res);
+
+// var MomGive = 1000;
+// var shopping = 700;
+
+// var res = MomGive - shopping;
+// console.log(res);
+
+
+
+// var math = 75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics = 35.45;
+// var bangla = 99.50;
+// var addi = math + biology + chemistry + physics + bangla;
+// var average = addi / 5;
+// var average = average.toFixed(2);
+// var average = parseFloat(average);
+// console.log(average);
+
+
+
+// var FirstOne = "I am going to be ";
+// var SecondOne = "an awesome web developer.";
+
+// console.log(FirstOne + SecondOne);
+
+// var Num1 = 119;
+// var Num2 = 5;
+// var result = Num1 % Num2;
+// console.log(result);
+
+
+
+
+
+//Arrayyyyyyyyyy
+
+
+// var friendsAge = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// var smartBoy = ["Mahin", "Sahin", "Kalam", "Salam", "Didar"]
+// console.log(smartBoy.length);
+
+//Index
+
+// var Num1 = [11, 22, 33, 44, 56.55];
+
+// Num1[1] = 85;
+// console.log(Num1[1]);
+
+
+
+
+//
+// var Num1 = [111,17111,1111,1111];
+// var New = Num1.indexOf(17111);
+// var numm = Num1.length;
+// console.log(numm);
+// //
+
+
+
+
+// var numbers = [78, 49, 59, 42, 93];
+// numbers.push(100);
+// console.log(numbers);
+
+// var singer = ["Kalam", "Salam", "Rahim", "Jannat"];
+// singer.push("Ashif");
+// console.log(singer);
+
+
+
+// var numbers = [78, 49, 59, 42, 93];
+// numbers.pop();
+// numbers.pop();
+// console.log(numbers);
+
+
+// console.log(5 != 5);
+// console.log(5 > 5);
+// console.log(5 < 5);
+// console.log(5 < 9);
+
+
+
+
