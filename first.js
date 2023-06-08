48485b47f623ccd0b13531579fa56080164ee6a6
AL SHAHRIAR HOSSAIN <https://github.com/UnlimitedFun39/My-First-Repo.git>
2023-06-09 00:55:02 +0600

    Js Update

--- first.js
+++ first.js
@@ -39,9 +39,17 @@
 // var bangla = 99.50;
 // var addi = math + biology + chemistry + physics + bangla;
 // var average = addi / 5;
-// var average = average.toFixed(2);
-// var average = parseFloat(average);
-// console.log(average);
+// var deci = average.toFixed(2);
+// var toNum = parseFloat(deci);
+// console.log(toNum);
+
+
+
+// var num = 119;
+// var thisNum = 5;
+// var res = num % thisNum;
+// console.log(res);
+
 
 
 
@@ -56,7 +64,8 @@
 // console.log(result);
 
 
-
+ 
+//////////////////////////////////Module---2  Javasript
 
 
 //Arrayyyyyyyyyy
@@ -95,6 +104,13 @@
 // console.log(singer);
 
 
+// var singer = ["Kalam", "Salam", "Rahim", "Jannat"];
+// singer.shift();
+// console.log(singer);
+
+
+
+
 
 // var numbers = [78, 49, 59, 42, 93];
 // numbers.pop();
@@ -106,6 +122,402 @@
 // console.log(5 > 5);
 // console.log(5 < 5);
 // console.log(5 < 9);
+
+
+
+
+// var iPhonePrice = 80000;
+// var budget = 15000;
+
+// if (iPhonePrice <= budget){
+//     console.log("iPhone Kena jabe");
+// } else {
+//     console.log("iPhone Kena jabena.");
+// }
+
+
+
+// var isGraduated = true;
+// var salery = 7000;
+// var car = true;
+
+// if ((isGraduated == true || car == true) && salery > 49000) {
+//     console.log("Biye pakka");
+// } else {
+//     console.log("Kopale Biye nai.");
+// }
+
+
+
+
+// var moneyIhave = 100;
+// var product1Price = 45;
+// var product2Price = 35;
+// var product3Price = 20;
+
+// if (product1Price < moneyIhave) {
+//     console.log("Khao 1");
+// } else if (product2Price < moneyIhave) {
+//     console.log("Khao 2");
+// } else if (product3Price < moneyIhave) {
+//     console.log("Khao 3");
+// } else {
+//     console.log("Khaoaa lagbena..");
+// }
+
+
+
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// console.log(fruits);
+// // check index of Banana
+// var indexOfBanana = fruits.indexOf('Banana');
+// console.log(indexOfBanana);
+
+// // replace banana by mango
+// fruits[1] = "Mango";
+// console.log(fruits);
+
+// // remove last element
+// var remove = fruits.pop();
+// console.log(fruits);
+
+// // add a element in last
+// var add = fruits.push('Watermelon');
+// console.log(fruits);
+
+
+
+
+// var myScore = 100;
+// var tomScore = 66;
+// var janeScore = 95;
+// var peterScore = 56;
+// var johnScore = 40;
+
+// if (myScore >= 80 && myScore <= 100) {
+//     console.log("Your Grade is A");
+// } else if (myScore >= 60 && myScore <= 79) {
+//     console.log("Your Grade is B");
+// } else if (myScore >= 50 && myScore <= 59) {
+//     console.log("Your Grade is C");
+// } else if (myScore >= 40 && myScore <= 49) {
+//     console.log("Your Grade is D");
+// } else  if(myScore >= 0  && myScore <= 39){
+//     console.log("I am fail");
+// }
+
+
+
+// var num1 = 131;
+// var num2 = 79;
+// var num3 = 99;
+
+// if (num1 > num2) {
+//     if (num1 > num3){
+//         console.log("Bigger Num is : " + num1)
+//     } else {
+//         console.log("Bigger Num is : " + num3)
+//     }
+// } else {
+//     if (num2 > num3) {
+//         console.log("Largest Number is : " + num2 )
+//     } else {
+//         console.log("Largest Number is : " + num3 )
+//     }
+// }
+
+
+
+// var side1 = 9;
+// var side2 = 8;
+// var side3 = 9;
+
+// if (side1 == side2 || side1 == side3 || side2 == side3) {
+//     console.log("Yess..");
+// } 
+
+
+
+
+
+
+
+
+
+
+// //////////////////////////////Module--3 Javasript
+
+
+
+
+
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//     console.log("Roarst Done");
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+
+
+// var num = 0;
+// while (num < 101) {
+//     console.log(num);
+//     num += 2;
+// }
+
+// var num = 1;
+// while (num < 101) {
+//     console.log(num);
+//     num += 2;
+// }
+
+
+
+// var num = 1;
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+
+
+
+
+
+// var num = 0;
+
+// for (var i = 0; i <= 10 ; i += 2) {
+//     console.log("Khabo  " + i);
+// }
+
+
+
+// var numbers = [10, 55, 25, 68, 97, 55, 15, 18, 58, 15, 56];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+
+
+// var items = ['Apple', 'Mango', 'Fuck', 'Banana', 'Orange', 'Jackfruit'];
+
+// for (var i = 0; i < items.length; i++) {
+//     var result = items[i];
+//     console.log(result);
+// }
+
+
+// for (var i = 0; i < 10; i++) {
+    
+//     if (i > 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// var items = ['Apple', 'Mango', 'Fuck', 'Banana', 'Orange', 'Jackfruit'];
+
+// for (var i = 0; i < items.length; i++) {
+//     var result = items[i];
+//     console.log(result);
+//     if (result == "Fuck" ) {
+//         break;
+//     }
+// }
+
+
+
+
+
+// var numbers = [10, 55, 25, 68, 97, 55, 15, 18, 58, 15, 56];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     if (number > 50) {
+//         continue;
+//     }
+//     console.log(number);
+// }
+
+
+
+
+
+
+
+
+// for (var i = 1; i < 40; i++) {
+//     console.log(i + ". Ajke amar mon bhalo nei.");
+// }
+
+
+
+
+
+//////////////////////////////   18.5.1    //////////////Practice
+
+// var Str = "jhsd";
+// var Num = 5;
+// var aRR = ['Hjd', 'sjdj', 'jhasdh'];
+// var Bol = true;
+
+// // += means aRR = aRR + Num;
+
+// var Num1 = 0.2 + 0.1;
+// var resul = parseInt(2);
+// console.log(resul);
+
+
+
+
+
+
+// var a = [153, 66, 33];
+// var b = a.length;
+//  console.log(b);
+
+// var c = a[0];
+//  console.log(c);
+
+
+// var moneyIhave = 81000;
+
+// if (moneyIhave >= 80000) {
+//     console.log("I will buy a Mackbook.");
+// } else if (moneyIhave >= 60000) {
+//     console.log("I will buy a Gaming Laptop.");
+// } else if (moneyIhave >= 40000) {
+//     console.log("I will buy a Lenovo Yoga");
+// } else if (moneyIhave >= 20000) {
+//     console.log("I will buy a second hand laptop");
+// } else {
+//     console.log("I will use my mobile");
+// }
+
+
+
+
+
+
+// for (var i = 1; i < 40; i++) {
+//     console.log(i + ". Ajke amar mon bhalo nei.");
+// }
+
+
+
+// for (var i = 58; i <= 98; i++) {
+//     console.log(i);
+// }
+
+
+// for (var i = 412; i <= 456; i += 2) {
+//     console.log(i);
+// }
+
+
+// for (var i = 581; i <= 623; i += 2) {
+//     console.log(i);
+// }
+
+
+// var topics = ['HTML', 'CSS', 'Tailwind CSS', 'Bootstrap'];
+// for (var i = 0; i < topics.length; i++){
+//     var main = topics[i];
+//     console.log(main);
+// }
+
+
+
+
+// var mobilePhones = ['Noakia', 'Motorolla', 'Xiomi', 'Samsung', 'Poco'];
+// var i = 0;
+// while (i < mobilePhones.length) {
+//     var main = mobilePhones[i];
+//     console.log(main);
+//     i++;
+// }
+
+
+
+
+// for (var i = 30; i <= 86; i++){
+//     if (i > 44) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// var BooksPrice = [150, 220, 165, 202, 158, 698, 189];
+// for (var i = 0; i < BooksPrice.length; i++){
+//     var main = BooksPrice[i];
+//     if (main > 200) {
+//         continue;
+//     } 
+//     console.log(main);
+// }
+
+
+
+
+
+// //////////////////////////////Module--4 Javascript
+
+
+
+// function StartFan() {
+    
+//     console.log("I am good boy.");
+// }
+
+// StartFan();
+
+
+
+// function add(a, b, c, d, e) {
+//     console.log(a, b, c, d, e);
+//     var addi = a+b+c+d+e;
+//     console.log(addi);
+// }
+
+// add(1, 2, 3, 4, 5);
+
+
+
+// function add(Num1, Num2) {
+//     console.log(Num1, Num2);
+//     var sum = Num1 + Num2;
+//     return sum;
+// }
+
+// var main = add(2, 3);
+// console.log(main);
+
+
+
+
+// function getAv (as1, as2, as3) {
+//     const total = as1 + as2+ as3;
+//     const average = total / 3;
+//     return average;
+// }
+// var Bangla = 50;
+// var Englush = 50;
+// var math = 60;
+// var getResult = getAv(Bangla, Englush, math);
+
+// console.log(getResult);
+
+
+
 
 
 
